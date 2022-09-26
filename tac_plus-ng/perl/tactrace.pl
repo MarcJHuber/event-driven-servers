15a88c42177de9f709de58d32e985c152087cc0e
Marc Huber <Marc.Huber@web.de>
2022-09-26 17:38:35 +0200

    tactrace: support authentication and accounting trace, too

--- tac_plus-ng/perl/tactrace.pl
+++ tac_plus-ng/perl/tactrace.pl
@@ -19,14 +19,17 @@ my $version = '$Id$';
 # our username = "demo";
 # 1; # <- this is mandatory
 # #
+our $mode = "authz";
 our $username = $ENV{"USER"};
+our $password = $ENV{"TACTRACEPASSWORD"}; # No CLI option, don't want this to be visible via ps
 our $port = "vty0";
 our $remote = "127.0.0.1";
 our $key = "demo";
 our $realm = "default";
 our $nad = "127.0.0.1";
-our $authentype = 1;
-our $authenmethod = 1;
+our $authentype = "ascii";
+our $authenmethod = "tacacsplus";
+our $authenservice = "login";
 our $exec = "/usr/local/sbin/tac_plus-ng";
 our $conf = "/usr/local/etc/tac_plus-ng.cfg";
 our $id = "tac_plus-ng";
@@ -35,27 +38,33 @@ our @args = ( "service=shell", "cmd*" );
 sub help {
 	my $arglist = '"' . join('" "', @args) . '"';
 	print <<EOT
-This is a TACACS+ authorization validator for $id.
+This is a TACACS+ AAA validator for $id.
 
 Usage: $0 [ <Options> ] [ <attributes> ... ]
 
-<attributes are the autorization AV pairs, default is:
+attributes are the autorization or accounting AV pairs, default is:
 	$arglist
 
 Options:
   --help		show this text
   --defaults=<file>	read default settings from <file>
+  --mode=<mode>		authc, authz or acct [$mode]
   --username=<username> username [$username]
   --port=<port>		port [$port]
   --remote=<client ip>	remote client ip [$remote]
   --key=<key>		encryption key [$key]
   --realm=<realm>	realm [$realm]
   --nad=<address>	NAD (router/switch/...) IP address [$nad]
-  --authentype=<n>	authen_type [$authentype]
+  --authentype=<type>	authen_type [$authentype]
   --authenmethod=<n>	authen_method [$authenmethod]
+  --authenservice=<n>	authen_method [$authenservice]
   --exec=<path>		executable path [$exec]
   --conf=<config>	configuration file [$conf]
   --id=<id>		id for configuration selection [$id]
+
+For authc the password can be set either via the environment variable
+TACTRACEPASSWORD or the defaults file. Setting it via a CLI option isn't
+supported as the password would show up as clear text in the process listing.
 
 Version: $version
 
@@ -86,14 +95,16 @@ sub read_defaults {
 GetOptions (
 	"help"		=> \&help,
 	"defaults=s"	=> \&read_defaults,
+	"mode=s"	=> \$mode,
 	"username=s"	=> \$username,
 	"port=s"	=> \$port,
 	"remote=s"	=> \$remote,
 	"key=s"		=> \$key,
 	"realm=s"	=> \$realm,
 	"nad=s"		=> \$nad,
-	"authentype=i"	=> \$authentype,
-	"authenmethod=i"=> \$authenmethod,
+	"authentype=s"	=> \$authentype,
+	"authenmethod=s"=> \$authenmethod,
+	"authenservice=s"=> \$authenservice,
 	"exec=s"	=> \$exec,
 	"conf=s"	=> \$conf,
 	"id=s"		=> \$id
@@ -102,6 +113,15 @@ GetOptions (
 @args = @ARGV if $#ARGV > -1;
 
 die "Can't access $conf" unless -r "$conf";
+
+my %Mode = ( "authc" => 1, "authz" => 2, "acct" => 3 );
+die "--mode=$mode unknown, supported args are " . join(", ", keys %Mode) unless exists $Mode{$mode};
+my %Authentype = ( "ascii" => 1, "pap" => 2 );
+die "--authentype=$authentype unknown, supported args are " . join(", ", keys %Authentype) unless exists $Authentype{$authentype};
+my %Authenmethod = ( "none" => 1, "krb5" => 2, "line" => 3, "enable" => 4, "local" => 5, "tacacsplus" => 6, "guest" => 8, "radius" => 0x10, "krb4" => 0x11, "rcmd" => 0x20 );
+die "--authenmethod=$authenmethod unknown, supported args are " . join(", ", keys %Authenmethod) unless exists $Authenmethod{$authenmethod};
+my %Authenservice = ( "login" => 1, "enable" => 2, "ppp" => 3, "pt"=> 5, "rcmd" => 6, "x25" => 7, "nasi" => 8 );
+die "--authenservice=$authenservice unknown, supported args are " . join(", ", keys %Authenservice) unless exists $Authenservice{$authenservice};
 
 # start tac_plus-ng:
 my($sock0, $sock1);
@@ -135,19 +155,25 @@ my $phdr = pack('W' x 16 . 'h*' . 'W' x $pad,
 	$fam, $famlen >> 8, $famlen & 0xff, $hexip, 0 x $pad);
 syswrite($conn0, $phdr, 16 + $famlen) or die "syswrite: $!";;
 
-# create a TACACS+ authorization packet and send it to tac_plus-ng:
+# create a TACACS+ $mode packet and send it to tac_plus-ng:
+
 my $pkt = Net::TacacsPlus::Packet->new(
-	'type' => 2,
+	'type' => $Mode{$mode},
 	'seq_no' => 1,
 	'session_id' => 1,
- 	'authen_method' => $authenmethod,
-	'authen_type' => $authentype,
+ 	'authen_method' => $Authenmethod{$authenmethod},
+	'authen_type' => $Authentype{$authentype},
 	'user' => $username,
 	'args' => \@args,
 	'key' => $key,
 	'remote' => $remote,
 	'port' => $port,
+	'authen_service' => $Authenservice{$authenservice},
+	'action' => 1,
+	'data' => $password,
+	'minor_version' => ($authentype eq "pap" && $mode eq "authc") ? 1 : 0
 );
+
 my $raw = $pkt->raw();
 syswrite($conn0, $raw, length($raw)) or die;
 sysread($conn0, my $buf, my $len = 1);
