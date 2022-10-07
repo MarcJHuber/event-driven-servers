a45d4804c0262d97cccb4590760ac22f37b40120
Marc Huber <Marc.Huber@web.de>
2022-10-07 18:00:36 +0200

    tac_plus-ng/mavis.c: add AV_A_PASSWORD_MUSTCHANGE attribute to debug output

--- tac_plus-ng/mavis.c
+++ tac_plus-ng/mavis.c
@@ -222,6 +222,7 @@ static void mavis_lookup_final(tac_session * session, av_ctx * avc)
 		    case AV_A_ROOT:
 		    case AV_A_SHELL:
 		    case AV_A_GIDS:
+		    case AV_A_PASSWORD_MUSTCHANGE:
 			if (avc->arr[i])
 			    fprintf(stderr, "  %-20s%s\n", av_char[i], avc->arr[i]);
 		    default:;
