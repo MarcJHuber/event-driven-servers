56ed22018faea5bd51899e1f46d06df31cee83ba
Marc Huber <Marc.Huber@web.de>
2022-12-18 09:52:11 +0100

    tac_plus-ng/headers.h: Drop support for OpenSSL < 3.0

--- tac_plus-ng/headers.h
+++ tac_plus-ng/headers.h
@@ -105,6 +105,11 @@
 #define MD5_LEN           16
 #define MSCHAP_DIGEST_LEN 49
 
+#if OPENSSL_VERSION_NUMBER < 0x30000000
+# undef WITH_SSL
+# warning Disabling OpenSSL support. Please upgrade to version 3.0
+#endif
+
 #ifdef WITH_SSL
 #undef WITH_TLS
 #endif
