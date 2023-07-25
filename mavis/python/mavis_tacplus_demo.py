d6f89a4611d1b082ba2a699845798e8a73404574
Marc Huber <Marc.Huber@web.de>
2023-07-25 18:01:27 +0200

    mavis/python/mavis_tacplus_demo.py: update

--- mavis/python/mavis_tacplus_demo.py
+++ mavis/python/mavis_tacplus_demo.py
@@ -5,28 +5,73 @@
 # (C)2023 Marc Huber <Marc.Huber@web.de>
 # All rights reserved.
 #
-# Demo TACACS+ backend for libmavis_external.so
-# Authenticates/authorizes user "demo" with password "demo".
+# Customizable TACACS+ backend for testing MAVIS functionality.
 #
 
 """
 Test input for authentication:
 0 TACPLUS
-4 $USER
-8 $PASS
+4 $USERNAME
+8 $PASSWORD
 49 AUTH
 =
 
-printf "0 TACPLUS\n4 $USER\n8 $PASS\n49 AUTH\n=\n" | this_script.py
+printf "0 TACPLUS\n4 demo\n8 demo\n49 AUTH\n=\n" | ./mavis_tacplus_demo.py
+
+#######
+
+Environment variables:
+
+USERNAME
+	Handled username.
+	Datault: "demo"
+
+PASSWORD
+	Handled password.
+	Datault: "demo"
+
+MEMBEROF
+	MEMBEROF attribute to return.
+	Datault: None
+
+TACMEMBER
+	TACMEMBER attribute to return.
+	Datault: None
+
+DELAY
+	Number of seconds a request will be delayed.
+	Default: 0
+
+ERROR
+	If set, the script will indicate an internal processing error.
+	Default: unset
+
+FAULT
+	If set, the script will exit during request processing.
+	Default: unset
 
 """
 
-import os, sys, re, ldap3
+import os, sys, re, time
 from mavis import ( Mavis,
 	MAVIS_DOWN, MAVIS_FINAL,
 	AV_V_RESULT_OK, AV_V_RESULT_ERROR, AV_V_RESULT_FAIL,
 	AV_V_RESULT_NOTFOUND
 )
+
+# A helper function to evaluate environment variables: #######################
+def eval_env(var, dflt):
+	globals()[var] = os.getenv(var) or dflt
+	return globals()[var]
+
+
+eval_env('USERNAME', 'demo')
+eval_env('PASSWORD', 'demo')
+eval_env('MEMBEROF', None)
+eval_env('TACMEMBER', None)
+eval_env('DELAY', '0')
+eval_env('ERROR', None)
+eval_env('FAULT', None)
 
 while True:
 	D = Mavis()
@@ -34,22 +79,29 @@ while True:
 	if not D.is_tacplus():
 		D.write(MAVIS_DOWN, None, None)
 		continue
+
 	if not D.valid():
 		D.write(MAVIS_FINAL, AV_V_RESULT_ERROR, "Invalid input.")
 		continue
 
-	if D.is_tacplus_authc and D.user == "demo" and D.password == "demo":
-		D.set_dbpassword(D.password)
-		D.set_memberof("\"cn=Demo,dc=demo\",\"cn=Sample,dc=demo\"")
-		D.set_tacmember("demogroup")
-		D.write(MAVIS_FINAL, AV_V_RESULT_OK, None)
+	time.sleep(float(DELAY))
+
+	if ERROR != None:
+		D.write(MAVIS_FINAL, AV_V_RESULT_ERROR, None)
 		continue
 
-	if D.is_tacplus_authz and D.user == "demo":
-		D.set_memberof("\"cn=Demo,dc=demo\",\"cn=Sample,dc=demo\"")
-		D.set_tacmember("demogroup")
-		D.write(MAVIS_FINAL, AV_V_RESULT_OK, None)
-		continue
+	if FAULT != None:
+		sys.stderr.write("Pretended application fault.\n")
+		sys.exit(-1);
+
+	if D.user == USERNAME:
+		if (D.is_tacplus_authc and D.password == PASSWORD) or D.is_tacplus_authz:
+			if MEMBEROF != None:
+				D.set_memberof(MEMBEROF)
+			if TACMEMBER != None:
+				D.set_tacmember(TACMEMBER)
+			D.write(MAVIS_FINAL, AV_V_RESULT_OK, None)
+			continue
 
 	D.write(MAVIS_DOWN, AV_V_RESULT_NOTFOUND, None)
 
