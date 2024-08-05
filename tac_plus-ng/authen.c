MarcJHuber/event-driven-servers

commit a957b970a60d9b6ca2002a489d7dc1955d112347

tac_plus-ng: implement  password fallback option, this closes #103

tac_plus-ng/authen.c
@@ -552,6 +552,14 @@ static int check_access(tac_session * session, struct pwdat *pwdat, char *passwd
 static void set_pwdat(tac_session * session, struct pwdat **pwdat, enum pw_ix *pw_ix)
 {
     if (session->user) {
+	if ((session->ctx->realm->last_backend_failure + session->ctx->realm->backend_failure_period > io_now.tv_sec)
+	    && session->ctx->host->authfallback == TRISTATE_YES) {
+	    if (*pw_ix == PW_LOGIN && session->user->passwd[PW_LOGIN_FALLBACK])
+		*pw_ix = PW_LOGIN_FALLBACK;
+	    else if (*pw_ix == PW_PAP && session->user->passwd[PW_PAP_FALLBACK])
+		*pw_ix = PW_PAP_FALLBACK;
+	}
+
 	*pwdat = session->user->passwd[*pw_ix];
 	if ((*pwdat)->type == S_login) {
 	    *pw_ix = PW_LOGIN;

tac_plus-ng/config.c
@@ -2297,6 +2297,19 @@ static void parse_password(struct sym *sym, tac_user * user)
 		parse_error_expect(sym, S_login, S_pap, S_chap, S_mschap, one ? S_unknown : S_closebra, S_unknown);
 	    }
 	    sym_get(sym);
+	    if (sym->code == S_fallback) {
+		switch (pw_ix) {
+		case PW_LOGIN:
+		    pw_ix = PW_LOGIN_FALLBACK;
+		    sym_get(sym);
+		    break;
+		case PW_PAP:
+		    pw_ix = PW_PAP_FALLBACK;
+		    sym_get(sym);
+		    break;
+		default:;
+		}
+	    }
 	    pp[pw_ix] = parse_pw(sym, user->memlist, cry);
 	    if (one)
 		break;

tac_plus-ng/headers.h
@@ -219,7 +219,7 @@ struct tac_net {
      BISTATE(visited);
 };
 
-enum pw_ix { PW_LOGIN = 0, PW_PAP, PW_CHAP, PW_MSCHAP, PW_MAVIS };
+enum pw_ix { PW_LOGIN = 0, PW_PAP, PW_CHAP, PW_MSCHAP, PW_LOGIN_FALLBACK, PW_PAP_FALLBACK, PW_MAVIS };
 
 enum hint_enum { hint_failed = 0, hint_denied, hint_nopass, hint_expired, hint_nosuchuser, hint_succeeded, hint_permitted, hint_no_cleartext,
     hint_backend_error, hint_denied_profile, hint_failed_password_retry, hint_bug, hint_abort, hint_denied_by_acl,

commit 81f704d8851c1c7392c78ccc861d40ce69fc0d0c

tac_plus-ng/authen.c: a957b970a60d9b6ca2002a489d7dc1955d112347 (issuer #103) broke fallback user handling. Thanks for reporting.

tac_plus-ng/authen.c
@@ -552,7 +552,7 @@ static int check_access(tac_session * session, struct pwdat *pwdat, char *passwd
 static void set_pwdat(tac_session * session, struct pwdat **pwdat, enum pw_ix *pw_ix)
 {
     if (session->user) {
-	if ((session->ctx->realm->last_backend_failure + session->ctx->realm->backend_failure_period > io_now.tv_sec)
+	if (!session->user->fallback_only && (session->ctx->realm->last_backend_failure + session->ctx->realm->backend_failure_period > io_now.tv_sec)
 	    && session->ctx->host->authfallback == TRISTATE_YES) {
 	    if (*pw_ix == PW_LOGIN && session->user->passwd[PW_LOGIN_FALLBACK])
 		*pw_ix = PW_LOGIN_FALLBACK;