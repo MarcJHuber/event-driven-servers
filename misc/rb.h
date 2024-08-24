98f9185bb0bd970001b0f5a5f0af61cc561e4b75
Marc Huber <Marc.Huber@web.de>
2024-08-24 19:18:42 +0200

    misc/rb.h: indent

--- misc/rb.h
+++ misc/rb.h
@@ -16,93 +16,107 @@
 #define rb_node_t rbnode_type
 #define rb_tree_t rbtree_type
 
-static inline __attribute__((always_inline)) rbtree_type *RB_tree_new(int (*cmpf)(const void *, const void *), void(*freef)(void *)) 
+static inline __attribute__((always_inline))
+rbtree_type *RB_tree_new(int (*cmpf)(const void *, const void *), void (*freef)(void *))
 {
-	return rbtree_create(cmpf, (const void *) freef);
+    return rbtree_create(cmpf,(const void *) freef);
 }
 
-static inline __attribute__((always_inline)) rbnode_type *RB_insert(rbtree_type *rbtree, void *data) 
+static inline __attribute__((always_inline))
+rbnode_type *RB_insert(rbtree_type * rbtree, void *data)
 {
-	rbnode_type *rbnode = calloc(1, sizeof(rbnode_type));
-	rbnode->key = data;
-	rbnode_type *rbnode_out = rbtree_insert(rbtree, rbnode);
-	if (rbnode_out)
-		return rbnode_out;
-	free(rbnode);
-	return NULL;
+    rbnode_type *rbnode = calloc(1, sizeof(rbnode_type));
+    rbnode->key = data;
+    rbnode_type *rbnode_out = rbtree_insert(rbtree, rbnode);
+    if (rbnode_out)
+	return rbnode_out;
+    free(rbnode);
+    return NULL;
 }
 
-static inline __attribute__((always_inline)) rbnode_type *RB_search(rbtree_type *rbtree, void *key) 
+static inline __attribute__((always_inline))
+rbnode_type *RB_search(rbtree_type * rbtree, void *key)
 {
-	return rbtree_search(rbtree, key);
+    return rbtree_search(rbtree, key);
 }
 
-static inline __attribute__((always_inline)) rbnode_type *RB_first(rbtree_type *rbtree) 
+static inline __attribute__((always_inline))
+rbnode_type *RB_first(rbtree_type * rbtree)
 {
-	return rbtree ? rbtree_first(rbtree) : NULL;
+    return rbtree ? rbtree_first(rbtree) : NULL;
 }
 
-static inline __attribute__((always_inline)) rbnode_type *RB_next(rbnode_type *rbnode) 
+static inline __attribute__((always_inline))
+rbnode_type *RB_next(rbnode_type * rbnode)
 {
-	return rbtree_next(rbnode);
+    return rbtree_next(rbnode);
 }
 
-static inline __attribute__((always_inline)) int RB_empty(rbtree_type *rbtree) 
+static inline __attribute__((always_inline))
+int RB_empty(rbtree_type * rbtree)
 {
-	return rbtree ? (rbtree->count == 0) : -1;
+    return rbtree ? (rbtree->count == 0) : -1;
 }
 
-static inline __attribute__((always_inline)) int RB_count(rbtree_type *rbtree) 
+static inline __attribute__((always_inline))
+int RB_count(rbtree_type * rbtree)
 {
-	return rbtree ? rbtree->count : 0;
+    return rbtree ? rbtree->count : 0;
 }
 
-void rbtree_freefunc(rbnode_type *rbnode, void *data);
+void rbtree_freefunc(rbnode_type * rbnode, void *data);
 
-static inline __attribute__((always_inline)) void RB_delete(rbtree_type *rbtree, rbnode_type *rbnode) 
+static inline __attribute__((always_inline))
+void RB_delete(rbtree_type * rbtree, rbnode_type * rbnode)
 {
-	rbtree_delete_node(rbtree, rbnode);
+    rbtree_delete_node(rbtree, rbnode);
+    if (rbtree->free)
+	rbtree->free((void *) rbnode->key);
+    free(rbnode);
+}
+
+static inline __attribute__((always_inline))
+const void *RB_delete_but_keep_data(rbtree_type * rbtree, rbnode_type * rbnode)
+{
+    rbtree_delete_node(rbtree, rbnode);
+    const void *ptr = rbnode->key;
+    free(rbnode);
+    return ptr;
+}
+
+static inline __attribute__((always_inline))
+void RB_tree_delete(rbtree_type * rbtree)
+{
+    if (rbtree) {
+	traverse_postorder(rbtree, rbtree_freefunc, rbtree);
+	free(rbtree);
+    }
+}
+
+static inline __attribute__((always_inline))
+void RB_search_and_delete(rbtree_type * rbtree, void *data)
+{
+    rbnode_type *rbnode = rbtree_delete(rbtree, data);
+    if (rbnode) {
 	if (rbtree->free)
-		rbtree->free((void *) rbnode->key);
+	    rbtree->free((void *) rbnode->key);
 	free(rbnode);
+    }
 }
 
-static inline __attribute__((always_inline)) const void *RB_delete_but_keep_data(rbtree_type *rbtree, rbnode_type *rbnode) 
+static inline __attribute__((always_inline))
+void *RB_lookup(rbtree_type * rbtree, void *data)
 {
-	rbtree_delete_node(rbtree, rbnode);
-	const void *ptr = rbnode->key;
-	free(rbnode);
-	return ptr;
+    rbnode_type *rbnode = rbtree_search(rbtree, data);
+    return rbnode ? (void *) rbnode->key : NULL;
 }
 
-static inline __attribute__((always_inline)) void RB_tree_delete(rbtree_type *rbtree) 
+static inline __attribute__((always_inline))
+void *RB_payload_get(rbnode_type * rbnode)
 {
-	if (rbtree) {
-		traverse_postorder(rbtree, rbtree_freefunc, rbtree);
-		free(rbtree);
-	}
+    return (void *) rbnode->key;
 }
 
-static inline __attribute__((always_inline)) void RB_search_and_delete(rbtree_type *rbtree, void *data) 
-{
-	rbnode_type *rbnode = rbtree_delete(rbtree, data);
-	if (rbnode) {
-		if (rbtree->free)
-			rbtree->free((void *) rbnode->key);
-		free(rbnode);
-	}
-}
-
-static inline __attribute__((always_inline)) void *RB_lookup(rbtree_type *rbtree, void *data) 
-{
-	rbnode_type *rbnode = rbtree_search(rbtree, data);
-	return rbnode ? (void *) rbnode->key : NULL;
-}
-
-static inline __attribute__((always_inline)) void *RB_payload_get(rbnode_type *rbnode) 
-{
-	return (void *) rbnode->key;
-}
 #define RB_payload(A,B) ((B)RB_payload_get(A))
 
 #endif				/* __RB_H__ */
