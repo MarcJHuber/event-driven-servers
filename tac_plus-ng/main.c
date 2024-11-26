8831d162cffb08584734feb19d4eca6e2e7c5cf3
Marc Huber <Marc.Huber@web.de>
2024-11-26 19:33:02 +0100

    tac_plus-ng/main.c: indent

--- tac_plus-ng/main.c
+++ tac_plus-ng/main.c
@@ -1252,7 +1252,7 @@ static void accept_control_common(int s, struct scm_data_accept_ext *sd_ext, soc
 
 static int query_mavis_host(struct context *ctx, void (*f)(struct context *))
 {
-    if (!ctx->host || ctx->host->try_mavis != TRISTATE_YES)
+    if(!ctx->host || ctx->host->try_mavis != TRISTATE_YES)
 	return 0;
     if (!ctx->mavis_tried) {
 	ctx->mavis_tried = 1;
