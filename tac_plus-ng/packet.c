bdd1ece5f5d4b34a92fc3e6009657183b705d626
Marc Huber <Marc.Huber@web.de>
2024-12-12 16:11:55 +0100

    tac_plus-ng/packet.c:tac_read() EAGAIN at the second read is actually valid, handle it correctly.

--- tac_plus-ng/packet.c
+++ tac_plus-ng/packet.c
@@ -617,6 +617,8 @@ void tac_read(struct context *ctx, int cur)
 	cleanup(ctx, cur);
 	return;
     }
+    if (len < 0 && errno == EAGAIN)
+	return;
     ctx->in->offset += len;
     if (ctx->in->offset != ctx->in->length)
 	return;
