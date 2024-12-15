90ec2ce77477f9a24e3d0eee33b2103d5bf4e79c
Marc Huber <Marc.Huber@web.de>
2024-12-15 09:40:10 +0100

    misc/str.h: add str_set()

--- misc/str.h
+++ misc/str.h
@@ -2,9 +2,16 @@
 #define __MISC_STR_H__
 
 #include <stddef.h>
+#include <string.h>
+
 typedef struct {
     char *txt;
     size_t len;
 } str_t;
 
+static inline void str_set(str_t *str, char *txt, size_t len)
+{
+	str->txt = txt;
+	str->len = len ? len : (txt ? strlen(txt) : 0);
+}
 #endif
