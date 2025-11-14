dc7a4a86e3b4c0867f530934ec3f089283435129
Marc Huber <Marc.Huber@web.de>
2025-11-14 14:17:16 +0100

    tac_plus-ng/mavis.c: double-quote profile attributes before sending them to the parser; this should fix #194

--- tac_plus-ng/mavis.c
+++ tac_plus-ng/mavis.c
@@ -322,10 +322,10 @@ static void mavis_lookup_final(tac_session *session, av_ctx *avc)
 
 		u->dynamic = io_now.tv_sec + r->caching_period;
 
-		if (parse_user_profile_multi(avc, &sym, u, "{ member = %.*s }", AV_A_TACMEMBER) ||
-		    parse_user_profile_multi(avc, &sym, u, "{ ssh-key = %.*s }", AV_A_SSHKEY) ||
-		    parse_user_profile_multi(avc, &sym, u, "{ ssh-key-hash = %.*s }", AV_A_SSHKEYHASH) ||
-		    parse_user_profile_multi(avc, &sym, u, "{ ssh-key-id = %.*s }", AV_A_SSHKEYID) ||
+		if (parse_user_profile_multi(avc, &sym, u, "{ member = \"%.*s\" }", AV_A_TACMEMBER) ||
+		    parse_user_profile_multi(avc, &sym, u, "{ ssh-key = \"%.*s\" }", AV_A_SSHKEY) ||
+		    parse_user_profile_multi(avc, &sym, u, "{ ssh-key-hash = \"%.*s\" }", AV_A_SSHKEYHASH) ||
+		    parse_user_profile_multi(avc, &sym, u, "{ ssh-key-id = \"%.*s\" }", AV_A_SSHKEYID) ||
 		    parse_user_profile_single(avc, &sym, u, "%.*s", AV_A_TACPROFILE)
 		    ) {
 
